4bb7c042c8667fb8fd6d815e96214ed7404ed0bc
Volker Kettenbach <volker@vicosmo.de>
2018-10-12 15:41:59 +0200

    Initial Checkin

--- 24_Iluminize.pm
+++ 24_Iluminize.pm
@@ -30,6 +30,8 @@ use warnings;
 use IO::Socket::INET;
 use IO::Socket::Timeout;
 
+use SetExtensions;
+
 
 my $on =   chr(hex("55")) .
     chr(hex("99")) .
@@ -63,12 +65,12 @@ sub Iluminize_Initialize($)
     my ($hash) = @_;
 
     $hash->{DefFn}      = "Iluminize_Define";
-    #$hash->{ReadFn}     = "Iluminize_Get"; # No get so far
+    $hash->{ReadFn}     = "Iluminize_Get";
     $hash->{SetFn}      = "Iluminize_Set";
     $hash->{UndefFn}    = "Iluminize_Undefine";
     $hash->{DeleteFn}   = "Iluminize_Delete";
     $hash->{AttrFn}     = "Iluminize_Attr";
-    $hash->{AttrList}   = $readingFnAttributes;
+    $hash->{AttrList}   = "$readingFnAttributes";
 }
 
 sub Iluminize_Define($$)
@@ -79,7 +81,9 @@ sub Iluminize_Define($$)
     my @a = split( "[ \t][ \t]*", $def );
     return "Wrong syntax: use define <name> Iluminize <hostname/ip> " if (int(@a) != 3);
 
-    Log3 $hash, 3, "Iluminize: $name defined.";
+    $hash->{HOST}=$a[2];
+
+    Log3 $hash, 0, "Iluminize: $name defined.";
 
     return undef;
 }
@@ -93,22 +97,35 @@ sub Iluminize_Set($$)
     my ($hash, $name, $cmd, @args) = @_;
     my $cmdList = "on off";
 
-    return "\"set $name\" needs at least one argument" unless(defined($cmd));
+    my $remote_host = $hash->{HOST};
+    my $remote_port = 8899;
 
     Log3 $hash, 3, "Iluminize: $name Set <". $cmd ."> called" if ($cmd !~ /\?/);
 
-    my $remote_host = $hash->{HOST};
-    my $remote_port = 8899;
+    return "\"set $name\" needs at least one argument" unless(defined($cmd));
+
+
+    my $command;
+    if ($cmd eq "on") {
+        $command = $on;
+        $hash->{STATE} = "on";
+    } elsif($cmd eq "off") {
+        $command = $off;
+        $hash->{STATE} = "off";
+    } else {
+        return SetExtensions($hash, $cmdList, $name, $cmd, @args);
+    }
 
     my $socket = IO::Socket::INET->new(PeerAddr => $remote_host,
         PeerPort => $remote_port,
         Proto    => 'tcp',
         Type     => SOCK_STREAM,
-        Timeout  => 1 )
+        Timeout  => 2 )
         or return "Couldn't connect to $remote_host:$remote_port: $@\n";
 
-    $socket->write($$cmd);
+    $socket->write($command);
 
+    return undef;
 }
 
 
@@ -116,25 +133,56 @@ sub Iluminize_Undefine($$)
 {
     my ($hash, $arg) = @_;
     my $name= $hash->{NAME};
-    Log3 $hash, 3, "Iluminize: $name undefined.";
+    Log3 $hash, 4, "Iluminize: $name undefined.";
     return;
 }
 
 
 
-sub Iluminize_Delete {
+sub Iluminize_Delete($$) {
     my ($hash, $arg) = @_;
     my $name= $hash->{NAME};
-    Log3 $hash, 3, "Ilumnize: $name deleted.";
+    Log3 $hash, 0, "Ilumnize: $name deleted.";
     return undef;
 }
 
 
 
-sub Iluminize_Attr {
+sub Iluminize_Attr($$$$) {
     my ($cmd, $name, $aName, $aVal) = @_;
     my $hash = $defs{$name};
+
+    return undef;
 }
 
+1;
 
 
+=pod
+=begin html
+
+<a name="Iluminize"></a>
+<h3>Iluminize</h3>
+  <br>
+    <a name="Iluminize"></a>
+    <b>Define</b>
+    <code>define &lt;name&gt; Iluminize &lt;ip/hostname&gt;</code><br>
+    	<br>
+    	Defines a wifi controlled Iluminize LED light
+
+=end html
+
+=begin html_DE
+<a name="Iluminize"></a>
+<h3>Iluminize</h3>
+  <br>
+    <a name="Iluminize"></a>
+    <b>Define</b>
+    <code>define &lt;name&gt; Iluminize &lt;ip/hostname&gt;</code><br>
+    	<br>
+    	Definiert ein Iluminize LED Wifi-Licht
+=end html_DE
+
+=item summary Support for Iluminize wifi controlled led stripe
+
+=item summary_DE Support für die Iluminize wlan LED-Produkte
---
+++ README.md
@@ -0,0 +1,23 @@
+# FHEM Support for Iluminize LED Stripes
+
+This implements are very basic support for Iluminize LED stripes.
+Currently only on/off is supported.
+
+These stripes can be purchased here https://www.iluminize.com/
+
+
+#  Requirements:
+   This module requires:
+   * Perl Module: IO::Socket::INET
+   * Perl Module: IO::Socket::Timeout
+
+  On a Debian (based) system, these requirements can be fullfilled by:
+
+   `apt-get install install libio-socket-inet6-perl libio-socket-timeout-perl`
+
+# Support
+
+
+#  Origin:
+  https://gitlab.com/volkerkettenbach/FHEM-Iluminize
+
